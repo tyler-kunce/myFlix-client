a477e38feb8aaba524ec33cdd39207ad605c4e8d
tyler-kunce <tyler.kunce@gmail.com>
2024-01-30 21:12:26 -0600

    Update movie-card.jsx
    Addition of prop-types

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
         <div
@@ -8,4 +10,14 @@ export const MovieCard = ({ movie, onMovieClick }) => {
             {movie.title}
         </div>
     );
+};
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        _id: PropTypes.string.isRequired,
+        title: PropTypes.string.isRequired,
+        description: PropTypes.string,
+        director: PropTypes.string
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired,
 };
