73499896cf341d61b7f1802b0cc6a8a30f5967d7
tyler-kunce <tyler.kunce@gmail.com>
2024-02-06 20:26:52 -0600

    Merge ex3-4
    Merge approved updates from task 3-4.

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
         <div
@@ -5,7 +7,17 @@ export const MovieCard = ({ movie, onMovieClick }) => {
                 onMovieClick(movie);
             }}
         >
-            {movie.title}
+            {movie.Title}
         </div>
     );
+};
+
+MovieCard.propTypes = {
+    movie: PropTypes.shape({
+        _id: PropTypes.string.isRequired,
+        Title: PropTypes.string.isRequired,
+        Description: PropTypes.string,
+        Director: PropTypes.string
+    }).isRequired,
+    onMovieClick: PropTypes.func.isRequired,
 };
