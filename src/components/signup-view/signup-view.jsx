bed07b96626eec9e9bc22917fec3882fcc445898
tyler-kunce <tyler.kunce@gmail.com>
2024-02-07 14:07:08 -0600

    Create signup-view.jsx
    Creation of Sign Up View for SPA

---
+++ src/components/signup-view/signup-view.jsx
@@ -0,0 +1,74 @@
+import { useState } from "react";
+
+export const SignUpView = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const { birthdate, setBirthdate } = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = {
+      Username: username,
+      Password: password,
+      Email: email,
+      Birthdate: birthday,
+    };
+
+    fetch("https://sports-movies-b0988f99dc86.herokuapp.com/signup", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: { "Content-Type": "application/json" },
+    }).then((response) => {
+      if (response.ok) {
+        alert("Signup successful");
+        window.location.reload();
+      } else {
+        alert("Signup failed");
+      }
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          required
+          minLength="5"
+        />
+      </label>
+      <label>
+        Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+      </label>
+      <label>
+        Birthdate:
+        <input
+          type="date"
+          value={birthdate}
+          onChange={(e) => setBirthdate(e.target.value)}
+        />
+      </label>
+      <button type="submit">Sign Up!</button>
+    </form>
+  );
+};
