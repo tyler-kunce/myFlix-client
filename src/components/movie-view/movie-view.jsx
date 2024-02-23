bf39de7a8620af386d9033e63905b9300680ee8f
tyler-kunce <tyler.kunce@gmail.com>
2024-02-22 21:40:54 -0600

    Client-side app routing
    Updated to include client-side app routing (nav-view, profile-view), update movie-card & movie-view to accept such

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -1,35 +1,41 @@
-import { UseParams, useParams } from 'react-router';
+import { useParams } from 'react-router';
+import { Row, Col, Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-export const MovieView = ({ movie }) => {
+export const MovieView = ({ movies }) => {
   const { movieId } = useParams();
-
-  const movie = movies.find((b) => b.id === movieId);
+  const movie = movies.find((movie) => movie._id === movieId);
 
   return (
-    <div>
-      <div>
-        <img src={movie.ImagePath} />
-      </div>
-      <div>
-        <span>Title: </span>
-        <span>{movie.Title}</span>
-      </div>
-      <div>
-        <span>Description: </span>
-        <span>{movie.Description}</span>
-      </div>
-      <div>
-        <span>Genre: </span>
-        <span>{movie.Genre}</span>
-      </div>
-      <div>
-        <span>Director: </span>
-        <span>{movie.Director}</span>
-      </div>
-      <Link to={`/`}>
-        <button className="back-button">Back</button>
-      </Link>
-    </div>
+    <>
+      <Row>
+        <Col md={5}>
+          <img src={movie.ImagePath} />
+        </Col>
+      </Row>
+      <Row>
+        <Col md={5}>
+          <div>
+            <span>Title: </span>
+            <span>{movie.Title}</span>
+          </div>
+          <div>
+            <span>Description: </span>
+            <span>{movie.Description}</span>
+          </div>
+          <div>
+            <span>Genre: </span>
+            <span>{movie.Genre}</span>
+          </div>
+          <div>
+            <span>Director: </span>
+            <span>{movie.Director}</span>
+          </div>
+          <Link to={`/`}>
+            <Button>Back</Button>
+          </Link>
+        </Col>
+      </Row>
+    </>
   );
 };
---
+++ src/components/navigation-view/navigation-view.jsx
@@ -0,0 +1,40 @@
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+export const NavigationBar = ({ user, onLoggedOut }) => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">
+          Great Sports Movies App
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse>
+          <Nav>
+            {user ? (
+              <>
+                <Nav.Link as={Link} to="/">
+                  Home
+                </Nav.Link>
+                <Nav.Link as={Link} to="/profile">
+                  Your Profile
+                </Nav.Link>
+                <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
+              </>
+            ) : (
+              <>
+                <Nav.Link as={Link} to="/login">
+                  Login
+                </Nav.Link>
+                <Nav.Link as={Link} to="/signup">
+                  Signup
+                </Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+        <Navbar.Collapse id="basic-navbar-nav"></Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
